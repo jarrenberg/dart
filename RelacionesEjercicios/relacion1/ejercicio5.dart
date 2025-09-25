a84b405c1e1896efd850bd9322ffe7bc06550780
Jorge <correoseguro696@gmail.com>
2025-09-25 20:56:02 +0200

    Signed-off-by: Jorge <>

---
+++ RelacionesEjercicios/relacion1/ejercicio5.dart
@@ -0,0 +1,11 @@
+/*5- Haz un pequeño programa en Dart en el que, a partir de las medidas de los tres lados de
+un triángulo (double) con valores asignados en la propia declaración, dictamine si éste es
+equilátero, isósceles o escaleno. */
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Por favor, ingrese su nombre: '); // Solicita el nombre al usuario
+  String? name = stdin.readLineSync(); // Lee la entrada del usuario
+  print ("$name");
+}
